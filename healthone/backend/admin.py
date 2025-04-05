ea097b764703086de24fe161da21a7c6c5a888d9
veerhan-glitch <veerhan-glitch>
2025-04-05 10:50:28 +0530

    fix admin

--- healthone/backend/admin.py
+++ healthone/backend/admin.py
@@ -5,9 +5,3 @@ admin.site.register(Test)
 admin.site.register(Lab)
 admin.site.register(Appointment)
 admin.site.register(LabTest)
-# Customize the admin interface for LabTest
-@admin.register(LabTest)
-class LabTestAdmin(admin.ModelAdmin):
-    list_display = ('id', 'lab', 'test', 'appointment', 'created_at')  # Adjust fields as per your model
-    search_fields = ('lab__name', 'test__name', 'appointment__id')  # Adjust fields as per your model
-    list_filter = ('lab', 'test', 'created_at')  # Adjust fields as per your model
